6da3f3f36fd3b3d582d4fcda28c2aba1d1437b5a
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2023-02-19 16:13:04 +1100

    fixed-errors

--- server.js
+++ server.js
@@ -10,15 +10,16 @@ const app = express ();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'./Develop/public')));
+const ROOT={root:path.join(__dirname,'./Develop/public')}
 
 app.get('/notes',(req, res)=>{
-    res.sendFile(path.join(__dirname,'/Develop/public/notes.html'));
+    res.sendFile('notes.html',ROOT);
 });
 
-app.get('*',(req, res) =>{
+/* app.get('*',(req, res) =>{
     res.sendFile(path.join(__dirname,'/Develop/public/index.html'));
-});
+}); */
 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname,'/Develop/db/db.json'));
@@ -26,7 +27,8 @@ app.get('/', (req, res) =>{
 
 app.get('/api/notes', (req, res) => {
     
-    res.json(`${req.method} request received to get notes`);
+    let noteList = JSON.parse(fs.readFileSync(path.join(__dirname,'/Develop/db/db.json'),'utf8'));
+    res.json(noteList)
   
     console.info(`${req.method} request received to get notes`);
   });
@@ -36,17 +38,33 @@ app.get('/api/notes', (req, res) => {
     console.info(`${req.method} request received to add a note`);
 
     let newNote = req.body;
-    let noteList = json.parse(fs.readFileSync('/Develop/db/db.json','utf8'));
-    let noteLength =(noteList.length).tostring();
+    let noteList = JSON.parse(fs.readFileSync(path.join(__dirname,'/Develop/db/db.json'),'utf8'));
+    let noteLength =noteList.length;
 
     newNote.id = noteLength;
     noteList.push(newNote);
     const noteString = JSON.stringify(noteList);
-    fs.writeFileSync('./Develop/db/db.json'), 
+    fs.writeFileSync(path.join(__dirname,'/Develop/db/db.json'),noteString);
     
     res.json(noteList);
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+   
+    const index=parseInt(req.params.id)
+   /*  res.json({test:index}) */
+   
+
+    let newNote = req.body;
+    let noteList = JSON.parse(fs.readFileSync(path.join(__dirname,'/Develop/db/db.json'),'utf8'));
+    noteList = noteList.filter(function(note, i) { return i !== index });
+    console.log(noteList);
+
+    const noteString = JSON.stringify(noteList);
+    fs.writeFileSync(path.join(__dirname,'/Develop/db/db.json'),noteString);
+    
+    res.json(noteList); 
+});
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
